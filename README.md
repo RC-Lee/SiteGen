b763cb5aaffb4b35098d1394799d9b3258170d9d
rclee91 <rlee85@myseneca.ca>
2021-09-17 03:39:34 -0400

    Feature and Readme updates

--- README.md
+++ README.md
@@ -1,17 +1,50 @@
-## SiteGen
+# SiteGen
 
-# About
-A basic Static Site Generator in release 0.1 with the following features.
+## About
+A basic Static Site Generator that converts .txt files to .html files. 
+Current release 0.1 with the following features.
 
-# 
+## Features
+ - (Required) Allows the user to specify an input file or folder to be processed.
+ - Allows the input to be a deep tree of files and folders, all .txt files are recursively parsed.
+ - Parsed titles (if any) from input files.
+ - Allows specification of a different output directory
+ - Allows specification of a stylesheet through linking a stylesheet URL
+ - Automatically generates an index.html file with relative links to each of the generated html files
 
-
-
-# Tool options
+## Tool options
 ```
-  --version    -v    Displays tool name and current version
-  --input      -i    Designate an input file or directory
-  --output     -o    Designate an output directory, default ./dist
-  --help       -h    Lists all available options
+  -v, --version                              Displays tool name and current version
+  -i, --input <file or directory name>       Designate an input file or directory
+  -o, --output <directory name>              Designate an output directory, default ./dist
+  -s, --stylesheet <stylesheet URL>          Link a stylesheet URL to the html
+  -h, --help                                 Lists all available options
 ```
+
+## How to use
+Note, this release has not yet been published on npm.
+In terminal/console head to the main sitegen directory and call
+`npm -install -g`
+
+### version option example
+`sitegen -v` or `sitegen --version` will display the tool name and current version.
+```
+  Name:  sitegen
+  Version:  0.1.0
+```
+
+### help option example
+`sitegen -h` or `sitegen --help` will list the tool options as listed above.
+
+### input option examples
+```
+ sitegen -i filename
+ sitegen --input "directory name"
+```
+This is the main processing option for the site generator tool
+The file or directory name must follow the input option.
+In the case of spaces in file or directory names, quotation marks "" are needed to wrap around the name.
+
+
+
 
--- bin/index.js
+++ bin/index.js
@@ -5,18 +5,23 @@ const fs = require('fs');
 const path = require('path');
 
 //path to current directory for dist folder
-const dist = process.cwd() + "/dist";
+const dist = "\\dist";
 let outputPath = dist;
 
 //tool options
 const options =[
-    {"long_option":"--version", "short_option":"-v", "description":"Displays tool name and current version"},
-    {"long_option":"--input", "short_option":"-i", "description":"Designate an input file or directory"},
-    {"long_option":"--output", "short_option":"-o", "description":"Designate an output directory, default ./dist"},
+    {"long_option":"--version", "short_option":"-v", "args":"", "description":"Displays tool name and current version"},
+    {"long_option":"--input", "short_option":"-i", "args":"<file or directory>", "description":"Designate an input file or directory"},
+    {"long_option":"--output", "short_option":"-o", "args":"<directory>",  "description":"Designate an output directory, default ./dist"},
+    {"long_option":"--stylesheet", "short_option":"-s", "args":"stylesheet URL",  "description":"Link a stylesheet URL"},
     {"long_option":"--help", "short_option":"-h", "description":"Lists all available options"},
 ]
 
 let m_args = process.argv.slice(2);
+let fileInd = 1;
+let stylesheetUrl = "";
+
+//determine and process options
 switch(m_args[0]){
     case '--version':
     case '-v':
@@ -25,49 +30,67 @@ switch(m_args[0]){
         break;
     case '--input':
     case '-i':
-        checkOutput().then(()=>{
-            checkInput();
+        checkArgs() //checks output and stylesheet options
+        .then(()=>{
+            processInput();
         }).catch((err)=>{console.log(err)});
         break;
     case '--help':
     case '-h':
         console.log(`Displaying tool options: `)
         options.forEach(option=>{
-            console.log(`  ${option.long_option.padEnd(12, ' ')} ${option.short_option.padEnd(5, ' ')} ${option.description}`);
+            console.log(`  ${option.short_option}, ${(option.long_option + " " + (option.args ? option.args : "")).padEnd(35, ' ')}  ${option.description}`);
         })
         break;
     default:
-        console.log("Unknown command, type --help or -h for command options");
+        let inputInd = m_args.findIndex(ele => ele == '--input' || ele == '-i');
+        if(inputInd == -1){
+            console.log("Unknown command, type --help or -h for command options");
+        }else{
+            fileInd = inputInd + 1;
+            checkArgs().then(()=>{
+                processInput();
+            }).catch((err)=>{console.log(err)});
+        }
+        
 }
 
 //Checks validity of input file or directory
-function checkInput(){
-    if(!m_args[1]){
+function processInput(){
+    if(!m_args[fileInd]){
         console.log("Input file or directory name required");
     } 
-    else if(!fs.existsSync(m_args[1])){
-        console.log(`Input file or directory ${m_args[1]} doesn't exist.`);
+    else if(!fs.existsSync(m_args[fileInd])){
+        console.log(`Input file or directory ${m_args[fileInd]} doesn't exist.`);
     }
     else
     {
         fs.access(m_args[1], fs.constants.R_OK, (err)=>{
             if(err)
-                console.log(`Can't access file or directory ${m_args[1]}`);
+                console.log(`Can't access file or directory ${m_args[fileInd]}`);
             else{
                 //Remove old output directory
                 fs.rmdir(outputPath, {recursive: true, force: true}, (err)=>{
                     if(err) throw err;
 
+                    //Create new output diretory
                     fs.mkdirSync(outputPath, {recursive: true});
-        
-                    let fStats = fs.statSync(m_args[1]);
+                    
+                    //Check if the input is a file or a directory
+                    let fStats = fs.statSync(m_args[fileInd]);
                     if(fStats.isFile()){
-                        createFile(m_args[1]);
+                        createFile(m_args[fileInd]);
                     } else if (fStats.isDirectory()){
-                        files = findTxTInDir(m_args[1], ".txt");
+                        let files = findInDir(m_args[fileInd], ".txt");
+                        let fileNames = [];
                         files.forEach(element => {
+                            fileNames.push(path.basename(element, '.txt'));
                             createFile(element);
                         });
+                        let indexContent = htmlContent(fileNames, "index");
+                        fs.writeFileSync(outputPath+"/"+"index.html", indexContent, (err)=>{
+                            if(err) throw err;
+                        })
                     }
                 });
             }
@@ -83,44 +106,63 @@ function createFile(filepath){
         let content = htmlContent(chunk.toString(), filename);
         fs.writeFileSync(outputPath+"/"+filename+".html", content, (err)=>{
             if(err) throw err;
-        })
+        });
     });
 }
 
+
 //Creates html content
 function htmlContent(data, filename){
-    let lines = data.split(/\r\n\r\n/);
-    let splitTitle = data.split(/\r\n\r\n\r\n/);
-    let hasTitle = false;
-    if(splitTitle.length > 1){
-        hasTitle = true;
-        lines.shift();
+    let title = "";
+    let lines = [];
+    if(typeof data === 'string'){
+        lines = data.split(/\r?\n\r?\n/);
+        let splitTitle = data.substr(0, 50).split(/\r?\n\r?\n\r?\n/);
+        if(splitTitle.length > 1){
+            title = splitTitle[0];
+            lines.shift();
+        }
+    } else{
+        title = "Index";
     }
-        
-    return `<!doctype html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>${hasTitle? splitTitle[0] : filename}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-    ${hasTitle ? `<h1>${splitTitle[0]}</h1>`: ""}
-    ${lines.map((ele)=>{
-        return  "<p>" + ele + "</p>\n\t\t" ;
-    }).join("")}
-    </body>
-    </html>`
+    
+    //Forming html with indents
+    let htmlContent = '<!doctype html>'+
+                        '\n\t<html lang="en">'+
+                        '\n\t<head>' +
+                        '\n\t\t<meta charset="utf-8">' +
+                        `\n\t\t<title>${title != "" ? title : filename}</title>` +
+                        `\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1">`+
+                        `${stylesheetUrl != "" ? `\n\t\t<link href=${stylesheetUrl} rel="stylesheet">` : ""}` +
+                        '\n\t</head>'+
+                        '\n\t<body>' +
+                        `${title != "" ? `<h1>${title}</h1>`: ""}`;
+
+    if(lines.length > 0){
+        lines.forEach(ele =>{
+            htmlContent += `\n\t\t<p>${ele}</p>`
+        });
+    }
+    else if(Array.isArray(data)){
+        data.forEach(ele =>{
+            if(ele)
+                htmlContent += `\n\t\t<h2>\n\t\t\t<a href="./${ele}.html">${ele}</a>\n\t\t</h2>`;
+        });
+    }
+    
+    htmlContent += '\n\t</body>\n</html>';
+
+    return htmlContent;
 }
 
-function findTxTInDir(filepath, extension){
+function findInDir(filepath, extension){
     let results =[];
     let files=fs.readdirSync(filepath);
     for(let i = 0; i < files.length; ++i){
         let filename = path.join(filepath,files[i]);
         let stat = fs.lstatSync(filename);
         if(stat.isDirectory()){
-            results = results.concat(findTxTInDir(filename, extension));
+            results = results.concat(findInDir(filename, extension));
         }
         else if(filename.indexOf(extension) >=0){
             results.push(filename);
@@ -129,16 +171,35 @@ function findTxTInDir(filepath, extension){
     return results;
 }
 
-function checkOutput(){
+//checks arguments for output or stylsheet options
+function checkArgs(){
     return new Promise((res, rej) =>{
-        if(m_args[2] == "--output" || m_args[2] == "-o"){
-            if(m_args[3]){
-                if(fs.existsSync(m_args[3])){
-                    outputPath = m_args[3];
+        let outputInd = m_args.findIndex(ele => (ele == '--output' || ele == '-o'));
+        let styleInd = m_args.findIndex(ele => (ele == '--stylesheet' || ele == '-s'));
+
+        //if --output or -o options exist
+        if(outputInd != -1){
+            // argument after the options should be the designated output directory
+            let outDirInd = outputInd + 1;
+            if(m_args[outDirInd]){
+                if(fs.existsSync(m_args[outDirInd])){
+                    outputPath = m_args[outDirInd];
                 }
                 else{
-                    console.log(`Output directory ${m_args[3]} doesn't exist, outputting all files to ./dist`);
+                    console.log(`Output directory ${m_args[outDirInd]} doesn't exist, outputting all files to ./dist`);
                 }
+            }
+        }
+
+        //if stylesheet option exists
+        if(styleInd != -1){
+            // argument after the option should be the stylesheet url
+            let styleUrlInd = styleInd + 1;
+            if(m_args[styleUrlInd]){
+                stylesheetUrl = m_args[styleUrlInd];
+            }
+            else{
+                console.log(`Stylesheet url doesn't exist, using default stylesheet`);
             }
         }
         res();
